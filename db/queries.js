72cf8d154547a304d0445b6dcbb3911554b1ae8e
Waron Limsakul <poonro@gmail.com>
2024-06-11 15:11:40 +0700

    change to store pool value set up in .env file

--- db/queries.js
+++ db/queries.js
@@ -1,10 +1,12 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
+
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "E-commerce api",
-  password: "postgres2",
-  port: 5433,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 ////////////////////////////////////////////////// Accounts
